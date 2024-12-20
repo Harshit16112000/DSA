f7db459d473b5b97c0947171a7efa8dfd918ef4a
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2024-12-20 15:48:00 +0530

    Time: 48 ms (39.66%) | Memory: 19 MB (42.86%) - LeetSync

---
+++ 3245-find-beautiful-indices-in-the-given-array-i/find-beautiful-indices-in-the-given-array-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> beautifulIndices(string s, string a, string b, int k) {
+        vector<int> matchA;
+        vector<int> matchB;
+
+        for(int i=0;(i+a.size()) <= s.size();i++)
+        {
+            if(s.substr(i,a.size()) == a)      matchA.push_back(i);
+        }
+        for(int i=0;(i+b.size()) <= s.size();i++)
+        {
+            if(s.substr(i,b.size()) == b)      matchB.push_back(i);
+        }
+
+        vector<int>result;
+        for(int i=0;i<matchA.size();i++)
+        {
+            auto it = lower_bound(matchB.begin(), matchB.end(), matchA[i]-k);
+            if ((it != matchB.end()) && (abs(*it - matchA[i]) <= k)) result.push_back(matchA[i]);
+        }
+        return result;
+    }
+};
