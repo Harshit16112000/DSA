4e531e42640ad1543a3c22c34397bfc596e330d2
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2024-12-23 11:08:58 +0530

    Time: 250 ms (91.67%) | Memory: 181.7 MB (82.57%) - LeetSync

--- 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.cpp
+++ 3594-identify-the-largest-outlier-in-an-array/identify-the-largest-outlier-in-an-array.cpp
@@ -10,6 +10,13 @@ public:
         std::unordered_map<int, int> mp;
         int ans = INT_MIN;
 
+        // Thought to sort the alforithm, and then check it in last two element, which is bigger,
+        // It didn't work out.
+        // Next Intuition is of 
+        // we need to take all sum and remove the current item ( outlier item) and then the sum should be double of one of the items in the considered items.
+        // One corner case is => when the double of the total sum is equal to the outlier item
+        //then in this case we need to check its freq => if greater then equal to 2 that means even we remove one of them as outlier we have at least one more which can be the sum of remaining.
+
         for (int num : nums) {
             totalSum += num;
             mp[num]++;
@@ -17,7 +24,8 @@ public:
 
         for (int num : nums) {
             int remainingSum = totalSum - num;
-            if (mp.find(remainingSum / 2) != mp.end() 
+            // The same element should not be present in Map. Think of that.
+             if (mp.find(remainingSum / 2) != mp.end() 
             && (remainingSum % 2 == 0) && ((remainingSum / 2 != num) || (mp[num] > 1))) {
                 ans = std::max(num, ans);
             }
