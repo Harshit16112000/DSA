49d3877653efc723cc8ae92a9d720f6aad0a5b72
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-01-04 16:13:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (66.51%) - LeetSync

--- 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.cpp
+++ 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.cpp
@@ -9,13 +9,13 @@ public:
             {
                 if(j-i+1 != n)  continue;
                 int l = 0;
-                bool match = true;
+                
                 for(int k=0;k<pattern.size();k++)
                 {
                     if(pattern[k] == 0 && nums[i+l] == nums[i+l+1])  l++;
                     else if(pattern[k] == 1 && nums[i+l] < nums[i+l+1]) l++;
                     else if(pattern[k] == -1 && nums[i+l] > nums[i+l+1]) l++;
-                    else match = false;
+                    
                 }
                 if(l == n-1)  ans++;
             }
