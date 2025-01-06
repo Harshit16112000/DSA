3798b394e85e2addb51ca8f0fe653e99ed5c306e
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-01-06 23:19:15 +0530

    Time: 6 ms (42.95%) | Memory: 17.3 MB (47.78%) - LeetSync

---
+++ 945-snakes-and-ladders/snakes-and-ladders.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int snakesAndLadders(vector<vector<int>>& board) {
+        int n = board.size();
+        vector<int> straightBoard(n * n + 1, -1);
+        bool leftToRight = true;
+        int idx = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (leftToRight) {
+                for (int j = 0; j < n; j++) {
+                    straightBoard[idx++] = board[i][j];
+                }
+            } else {
+                for (int j = n - 1; j >= 0; j--) {
+                    straightBoard[idx++] = board[i][j];
+                }
+            }
+            leftToRight = !leftToRight;
+        }
+
+            
+            vector<int> vis(n*n+1, 0);
+            queue<pair<int,int>> q;
+            q.push({1,0});
+            int mini = 0;
+            vis[1] = 1;
+            while(!q.empty())
+            {
+                auto it = q.front();
+                q.pop();
+                int curr = it.first;
+                int move = it.second;
+                if(curr == n*n)  {
+                    return move;
+                }
+                
+                for(int i=1; i <= 6;i++)
+                {
+                    int next = curr + i;
+                    if(next > n*n) break;
+                    
+                    if(!vis[next] ) {
+                        vis[next] = 1;
+                        if(straightBoard[next] != -1)
+                        {
+                            q.push({straightBoard[next], move + 1});
+                        }
+                        else {
+                            q.push({next, move + 1});
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+    
+};
