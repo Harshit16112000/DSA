601773c110bf21ef2589bb1f1ac484a855c0cd6a
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-01-14 19:19:15 +0530

    Time: 1278 ms (5.05%) | Memory: 402.5 MB (5.02%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -1,37 +1,45 @@
 class Solution {
 public:
-    long long dp[501][501][3];
-    bool visited[501][501][3];
-    long long helper(int i, int j, int count, vector<vector<int>>& coins) {
-        int n = coins.size(), m = coins[0].size();
-        if (i == coins.size() - 1 && j == coins[0].size() - 1) {
-            if (coins[i][j] < 0 && count > 0)
-                return 0;
-            return coins[i][j];
-        }
+    long long solve(int i, int j, vector<vector<int>>& coins,
+              vector<vector<vector<int>>>& dp, int k,
+              vector<vector<vector<int>>>& visited) {
+
         if (i >= coins.size() || j >= coins[0].size())
             return INT_MIN;
 
-        if (visited[i][j][count])
-            return dp[i][j][count];
+        if (i == coins.size() - 1 && j == coins[0].size() - 1) {
+            if (coins[i][j] < 0 && k > 0)
+                return 0;
+            return coins[i][j];
+        }
+        if (visited[i][j][k])
+            return dp[i][j][k];
 
         long long right = INT_MIN, down = INT_MIN;
 
-        right = coins[i][j] + helper(i, j + 1, count, coins);
-        if (count > 0 && coins[i][j] < 0) {
-            right = max(right, helper(i, j + 1, count - 1, coins));
+        right = coins[i][j] + solve(i, j + 1, coins, dp, k, visited);
+        if (k > 0 && coins[i][j] < 0) 
+        {
+            right = max(right, solve(i, j + 1,coins, dp, k - 1, visited));
         }
 
-        down = helper(i + 1, j, count, coins) + coins[i][j];
-        if (count > 0 && coins[i][j] < 0)
-            down = max(down, helper(i + 1, j, count - 1, coins));
+        down = solve(i + 1, j, coins, dp, k, visited) + coins[i][j];
+        if (k > 0 && coins[i][j] < 0)
+            down = max(down, solve(i + 1, j,coins, dp, k - 1, visited));
 
-        visited[i][j][count] = 1;
-        return dp[i][j][count] = max(right, down);
+        visited[i][j][k] = 1;
+        return dp[i][j][k] = max(right, down);
     }
+
     int maximumAmount(vector<vector<int>>& coins) {
-        memset(dp, -1, sizeof(dp));
-        memset(visited, 0, sizeof(visited));
-        return helper(0, 0, 2, coins);
+        int m = coins.size();
+        int n = coins[0].size();
+
+        vector<vector<vector<int>>> dp(
+            m, vector<vector<int>>(n, vector<int>(3, -1)));
+        vector<vector<vector<int>>> visited(
+            m, vector<vector<int>>(n, vector<int>(3, 0)));
+
+        return solve(0, 0, coins, dp, 2, visited);
     }
 };
