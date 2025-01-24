aec510bd98925b112b7ef174481676f7b9155419
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-01-25 00:05:25 +0530

    Time: 4 ms (92.06%) | Memory: 51.4 MB (94.94%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,48 +1,44 @@
 class Solution {
+private:
+    bool isCyclic(int node, vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& pathVisited, vector<bool>& isSafe) {
+        visited[node] = true;
+        pathVisited[node] = true;
+        
+        for (int ne : graph[node]) {
+            if (!visited[ne]) {
+                if (isCyclic(ne, graph, visited, pathVisited, isSafe)) {
+                    return true;
+                }
+            } else if (pathVisited[ne]) {
+                return true;
+            }
+        }
+        
+        pathVisited[node] = false;
+        isSafe[node] = true;
+        return false;
+    }
+
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        // Question Comes up to look for Cyclic Component in DAG.
-        // Apart from Cyclic Component everything is ans.
-        // IN DAG, best way is Topo Sort.
-        int n = graph.size();
-        vector<int> inDegree(n, 0);
-        vector<int> ans;
-        vector<vector<int>> revGraph(n);
-        queue<int> q;
-        for (int i = 0; i < n; i++) {
-            for (auto it : graph[i]) {
-                revGraph[it].push_back(i);
-                inDegree[i]++;
+        int nodeCount = graph.size();
+        vector<bool> visited(nodeCount, false);
+        vector<bool> pathVisited(nodeCount, false);
+        vector<bool> isSafe(nodeCount, false);
+        
+        for (int node = 0; node < nodeCount; node++) {
+            if (!visited[node]) {
+                isCyclic(node, graph, visited, pathVisited, isSafe);
             }
         }
-
-        for (int i = 0; i < inDegree.size(); i++) {
-            cout << inDegree[i] << " ";
-        }
-
-        for (int node = 0; node < n; node++) {
-                        if (inDegree[node] == 0) {
-                                q.push(node);
-                ans.push_back(node);
-                           
-            }
-                   
-        }
-
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            for (auto it : revGraph[node]) {
-
-                inDegree[it]--;
-                if (inDegree[it] == 0) {
-                    ans.push_back(it);
-                    q.push(it);
-                }
+        
+        vector<int> safeNodes;
+        for (int node = 0; node < nodeCount; node++) {
+            if (isSafe[node]) {
+                safeNodes.push_back(node);
             }
         }
-
-        sort(ans.begin(), ans.end());
-        return ans;
+        
+        return safeNodes;
     }
 };
