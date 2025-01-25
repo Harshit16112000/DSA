51caa5136f91872be3ba7eec536830d7b55cb6b9
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-01-25 11:28:57 +0530

    Time: 203 ms (61.49%) | Memory: 179.4 MB (55.40%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,53 +1,25 @@
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-        // Store the indices of each number
-        multimap<int, int> mp; // Use multimap to handle duplicates
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            mp.insert({nums[i], i});
+        vector<pair<int, int>> indexed;
+        for (int i = 0; i < nums.size(); ++i) indexed.push_back({nums[i], i});
+        sort(indexed.begin(), indexed.end());
+
+        vector<vector<int>> groups = {{indexed[0].second}};
+        for (int i = 1; i < nums.size(); ++i) {
+            if (indexed[i].first - indexed[i - 1].first <= limit)
+                groups.back().push_back(indexed[i].second);
+            else
+                groups.push_back({indexed[i].second});
         }
 
-        vector<int> ans(n, -1);
-        vector<int> sortedarr;    // Current group of sorted numbers
-        vector<int> IndexArr;     // Indices corresponding to the current group
-
-        auto it = mp.begin(); // Iterate through sorted numbers
-        while (it != mp.end()) {
-            int current_val = it->first; // Current value in sorted order
-            int current_idx = it->second; // Original index of this value
-            
-            // Missed to check with sortedarr.empty()
-            if (sortedarr.empty() || current_val - sortedarr.back() <= limit) {
-                // Add to current group
-                sortedarr.push_back(current_val);
-                IndexArr.push_back(current_idx);
-            } else {
-                // Process current group and start a new one
-                sort(IndexArr.begin(), IndexArr.end());
-                for (int i = 0; i < sortedarr.size(); i++) {
-                    ans[IndexArr[i]] = sortedarr[i];
-                }
-                sortedarr.clear();
-                IndexArr.clear();
-                // Start a new group
-
-                // I have missed to add this part
-                sortedarr.push_back(current_val);
-                IndexArr.push_back(current_idx);
-            }
-            it++;
+        for (auto& group : groups) {
+            vector<int> sortedVals;
+            for (int i : group) sortedVals.push_back(nums[i]);
+            sort(sortedVals.begin(), sortedVals.end());
+            sort(group.begin(), group.end());
+            for (int i = 0; i < group.size(); ++i) nums[group[i]] = sortedVals[i];
         }
-
-        // Handle the last group
-        // ALso, missed this part
-        if (!sortedarr.empty()) {
-            sort(IndexArr.begin(), IndexArr.end());
-            for (int i = 0; i < sortedarr.size(); i++) {
-                ans[IndexArr[i]] = sortedarr[i];
-            }
-        }
-
-        return ans;
+        return nums;
     }
 };
