fc30bff2f976938913f24f9a443a00d690731a8d
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-01-27 23:12:08 +0530

    Time: 4 ms (72.07%) | Memory: 19.2 MB (86.23%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool canFinish(int V, vector<vector<int>>& pre) {
+        vector<vector<int>> adj(V);
+           for(int i = 0 ; i < pre.size() ; i++){
+            adj[pre[i][1]].push_back(pre[i][0]);
+        }
+
+				vector<int> indegree(V,0);
+
+      
+	    for(int i=0;i<V;i++)
+	    {
+	        for(auto it: adj[i])
+	        indegree[it]++;
+	    }
+	    
+	    queue<int> q;
+	    vector<int>topo;
+        for(int i=0;i<V;i++)
+	    {
+	        if(indegree[i] == 0)
+	            {
+                    q.push(i);
+                    topo.push_back(i);
+                }
+	    }
+	    
+	    
+	    while(!q.empty())
+	    {
+	        int node = q.front();
+	        q.pop();
+	        
+	        for(auto it: adj[node])
+	        {
+	            indegree[it]--;
+	            if(indegree[it] == 0)
+                {
+	              q.push(it);
+                  topo.push_back(it);
+                }
+	        }
+	    }
+	    if (topo.size() == V)
+         return true;
+        return false;
+    }
+};
