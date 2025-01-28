85fe7d19a9ae0b75ebe6f588c6fe483c6398a91c
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-01-28 19:59:50 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (49.43%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     void DFS(int row, int col, vector<vector<int>>& vis,
-             vector<vector<int>>& grid, int& ans, int value) {
+             vector<vector<int>>& grid, int& ans) {
         vis[row][col] = 1;
         ans++;
 
         int dr[] = {-1, 0, 1, 0};
         int dc[] = {0, -1, 0, 1};
 
-        if (value == 0) {
-            grid[row][col] = 0;
-        }
+        
 
         for (int i = 0; i < 4; i++) {
             int nr = row + dr[i];
@@ -18,7 +16,7 @@ public:
 
             if (nr >= 0 && nr < grid.size() && nc >= 0 && nc < grid[0].size() &&
                 !vis[nr][nc] && grid[nr][nc] > 0) {
-                DFS(nr, nc, vis, grid, ans, value);
+                DFS(nr, nc, vis, grid, ans);
             }
         }
     }
@@ -31,14 +29,11 @@ public:
         int maxi = 0;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                
-                    if (grid[i][j] > 0 && !vis[i][j]) {
-                        int ans = 0;
-                        int value = 1;
-                        DFS(i, j, vis, grid, ans, value);
-                        maxi = max(maxi, ans);
-                    }
-                
+                if (grid[i][j] > 0 && !vis[i][j]) {
+                    int ans = 0;
+                    DFS(i, j, vis, grid, ans);
+                    maxi = max(maxi, ans);
+                }
             }
         }
 
