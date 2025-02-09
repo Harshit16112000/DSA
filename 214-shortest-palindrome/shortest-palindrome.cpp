5a6487652ab722e69a3db56dc3662801b3cfd390
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-02-09 21:23:12 +0530

    Time: 3 ms (73.21%) | Memory: 16.7 MB (5.26%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
+    vector<int> getKmp(string s, string a)
+    {
+        vector<int> lps(s.size(), 0);
+        for(int i=1;i<s.size();i++)
+        {
+            int prevIndx = lps[i-1];
+            while(prevIndx > 0 && s[i] != s[prevIndx])
+            {
+                prevIndx = lps[prevIndx-1];
+            }
+            lps[i] = prevIndx + (s[i] == s[prevIndx] ? 1 : 0);
+        }
+        
+            return lps;
+        
+    }
     string shortestPalindrome(string s) {
         string rev = s;
         reverse(rev.begin(), rev.end());
+        string s1 = s + '#' + rev;
+        vector<int>lps = getKmp(s1,rev);
 
-        for(int i=0;i<s.size();i++)
+        // for(int i=0;i<lps1.size();i++)
+        // {
+        //     cout << lps1[i] << " ";
+        // }
+
+        string ans = "";
+        for(int i = 0; i < rev.size() - lps[lps.size() -1];i++)
         {
-            // if(s.substr(0,s.size()-i) == rev.substr(i))
-            // {
-            //     return rev.substr(0,i) + s;
-            // }
-
-            if(memcmp(s.c_str(), rev.c_str() + i , s.size()-i) == 0)
-            {
-                return rev.substr(0,i) + s;
-            }
+            ans += rev[i];
         }
 
-        return rev + s;
+        return ans + s;
     }
 };
