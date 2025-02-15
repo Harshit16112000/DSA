c07d3d58de14677fba103e405c28f8bbb05ac367
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-02-15 23:40:27 +0530

    Time: 176 ms (26.67%) | Memory: 186.4 MB (16.67%) - LeetSync

---
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<double> getArea(vector<vector<int>> &squares,  double mid)
+    {
+        double lower = 0;
+        double upper = 0;
+
+        for(auto &s: squares)
+        {
+            if(s[1] + s[2] <= mid)   lower += (double)s[2] * (double)s[2];
+            else if(s[1] >= mid)     upper += (double)s[2] * (double)s[2];
+            else {
+                lower += (mid - (double)s[1]) * (double)s[2];
+                upper += ((double)s[1] + (double)s[2] - mid)*(double)s[2]; //(double)s[2] * (double)s[2] - lower;
+            }
+        }
+
+        return {lower, upper};
+    }
+    double separateSquares(vector<vector<int>>& squares) {
+        double low = 0;  double mid = 0; double high = 0;
+        double ans = 0;
+         high = INT_MAX;
+
+        while(low <= high)
+        {
+            mid = (low) + (high-low)/2;
+            vector<double> area = getArea(squares, mid);
+            double lowerArea = area[0];
+            double upperArea = area[1];
+
+            if(ans == mid)  break;
+            if(upperArea > lowerArea)  low = mid;
+            else high = mid ;
+            ans = mid;
+        }
+
+        return ans;
+    }
+};
