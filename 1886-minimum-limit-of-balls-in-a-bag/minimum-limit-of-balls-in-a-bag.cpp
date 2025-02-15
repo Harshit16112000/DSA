10d198e3b04508a72fd792aa33b831cb24957341
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-02-15 18:03:45 +0530

    Time: 85 ms (5.14%) | Memory: 122.3 MB (5.40%) - LeetSync

--- 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
@@ -6,8 +6,14 @@ public:
         int cnt = 0;
         for(int i=0;i<nums.size();i++)
         {
-            // Done as 5 is deivided into 2,3 where cnt is 2, but here we need the cnt as 1.    
-            cnt += (nums[i] - 1) / mid;
+            // Done as 5 is deivided into 2,3 where cnt is 2, but here we need the cnt as 1.          
+            // As, 7/5 result in 1. To need floating value. we need to do (double)
+
+            cnt +=  ceil((double)nums[i]/mid);
+            cnt--;
+            
+            // Other way to do it 
+            // cnt += (nums[i] -1)/mid;
             if(cnt > maxOperation)  return false;
         }
         return true;
@@ -16,6 +22,7 @@ public:
     int minimumSize(vector<int>& nums, int maxOperations) {
         int l = 1;
         int r = 0;
+        cout << 7/5 << " " << 17/5 << " ";
         for(int i=0;i<nums.size();i++) 
         {
             r = max(r, nums[i]);
