b8a8dee618111711b91e40495a23d7fac3fd5754
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-03-09 11:02:45 +0530

    Time: 381 ms (12.50%) | Memory: 212.2 MB (12.50%) - LeetSync

---
+++ 3759-choose-k-elements-with-maximum-sum/choose-k-elements-with-maximum-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<long long> findMaxSum(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size();
+        map<int, vector<int>> indices; 
+        for (int i = 0; i < n; i++) 
+            indices[nums1[i]].push_back(i);
+
+        vector<long long> ans(n, 0);
+        priority_queue<int, vector<int>, greater<int>> minHeap; 
+        long long sum = 0;
+
+        for (auto& [value, indexList] : indices) {
+                for (int index : indexList) {
+                if (indexList[0] != index) {
+                    ans[index] = ans[indexList[0]];
+                } else {
+                    ans[index] = sum;  // Store the sum of top k elements
+                }
+            }
+
+            for (int index : indexList) {
+                minHeap.push(nums2[index]);
+                sum += nums2[index];
+
+                if (minHeap.size() > k) {
+                    sum -= minHeap.top();
+                    minHeap.pop();
+                }
+            }
+        }
+        return ans;
+    }
+};
