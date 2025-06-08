76414aef75aca7d67b2247f492bce870193acd26
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-06-08 19:37:31 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (51.26%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
-    int solve(int row, int col,vector<vector<int>>& triangle, vector<vector<int>>& dp )
-    {
-        if(row == triangle.size()-1)
-           return triangle[row][col];
-        
-        if (dp[row][col] != -1)
-             return dp[row][col];
-        int row1 =  triangle[row][col] + solve(row+1,col, triangle,dp) ;
-        int row2 =  triangle[row][col] + solve(row+1,col+1, triangle,dp);
-        return dp[row][col] =  min(row1,row2);
-        //return dp[row][col];
+    int solve(int row, int col, vector<vector<int>>& triangle,
+              vector<vector<int>>& dp) {
+        if (row == triangle.size() - 1)
+            return triangle[row][col];
 
+        if (dp[row][col] != -1)
+            return dp[row][col];
+        int row1 = triangle[row][col] + solve(row + 1, col, triangle, dp);
+        int row2 = triangle[row][col] + solve(row + 1, col + 1, triangle, dp);
+        return dp[row][col] = min(row1, row2);
+        // return dp[row][col];
     }
 
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>> dp(triangle.size(), vector<int> (triangle.size(), -1));
+        int n = triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
 
-        return solve(0,0,triangle,dp);
+        dp[0][0] = triangle[0][0];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < triangle[i].size(); j++) {
+                if (j < triangle[i - 1].size())
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j] + triangle[i][j]);
+
+                if (j - 1 >= 0)
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + triangle[i][j]);
+            }
+        }
+
+        int mini = INT_MAX;
+        for (int i = 0; i < triangle.size(); i++) {
+            mini = min(mini, dp[n - 1][i]);
+        }
+
+        return mini;
+        // return solve(0,0,triangle,dp);
     }
-
-    
-
 };
