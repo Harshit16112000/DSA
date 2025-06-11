a87522dea163d18e48380f059bf8af39c94fa8c1
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-06-11 22:16:08 +0530

    Time: 122 ms (42.11%) | Memory: 50.1 MB (18.60%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int solve(int i, int sum, vector<int>& nums, vector<unordered_map<int, int>>& dp) {
-        if (i == nums.size()) {
-            return sum == 0 ? 1 : 0;
-        }
-        if (dp[i].count(sum)) return dp[i][sum];
-
-        int add = solve(i + 1, sum - nums[i], nums, dp);
-        int subtract = solve(i + 1, sum + nums[i], nums, dp);
-
-        return dp[i][sum] = add + subtract;
-    }
-
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<unordered_map<int, int>> dp(n);
-        return solve(0, target, nums, dp);
+
+        // dp[i][sum] = number of ways to make 'sum' using first 'i' elements
+        vector<unordered_map<int, int>> dp(n + 1);
+        dp[0][0] = 1;  // base case: one way to make sum=0 with 0 elements
+
+        for (int i = 0; i < n; i++) {
+            for (auto& [currSum, count] : dp[i]) {
+                int add = currSum + nums[i];
+                int subtract = currSum - nums[i];
+                dp[i + 1][add] += count;
+                dp[i + 1][subtract] += count;
+            }
+        }
+
+        return dp[n][target];
     }
 };
