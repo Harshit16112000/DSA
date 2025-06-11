240ce7abc733b91d68cd34003e12e4aef653ea9e
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-06-11 22:02:22 +0530

    Time: 381 ms (27.84%) | Memory: 89.9 MB (5.46%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-    int dpProblem(int index, int target, vector<int>& nums, vector<vector<int>>& dp, int offset, int maxSum) {
-        // If out of range after applying offset
-        if (target + offset < 0 || target + offset > maxSum) return 0;
-
+    int dpProblem(int index, int target, vector<int>& nums, unordered_map<string, int>& memo) {
         if (index == nums.size()) {
             return target == 0 ? 1 : 0;
         }
 
-        if (dp[index][target + offset] != -1) {
-            return dp[index][target + offset];
+        string key = to_string(index) + "|" + to_string(target);
+
+        if (memo.find(key) != memo.end()) {
+            return memo[key];
         }
 
-        int add = dpProblem(index + 1, target - nums[index], nums, dp, offset, maxSum);
-        int subtract = dpProblem(index + 1, target + nums[index], nums, dp, offset, maxSum);
+        int add = dpProblem(index + 1, target - nums[index], nums, memo);
+        int subtract = dpProblem(index + 1, target + nums[index], nums, memo);
 
-        return dp[index][target + offset] = add + subtract;
+        return memo[key] = add + subtract;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
-
-        int offset = sum;
-        int maxSum = 2 * sum;
-
-        if (target > sum || target < -sum) return 0;
-
-        vector<vector<int>> dp(nums.size(), vector<int>(maxSum + 1, -1));
-
-        return dpProblem(0, target, nums, dp, offset, maxSum);
+        unordered_map<string, int> memo;
+        return dpProblem(0, target, nums, memo);
     }
 };
