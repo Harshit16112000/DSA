28c7f32f8a9daa35b5c57914042e76a0e6c4ec3b
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-06-18 22:52:38 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (21.35%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -18,7 +18,7 @@ public:
         int n = word1.size();
         int m = word2.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        vector<vector<int>> dp(n, vector<int>(m, -1));
 
         return dpProblem(0,0,word1,word2,dp);
     }
