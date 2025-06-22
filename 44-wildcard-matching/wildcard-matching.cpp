7c25738e6be4324bda46e228d45881b01a78bc7f
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2025-06-22 20:57:39 +0530

    Time: 38 ms (26.75%) | Memory: 14.7 MB (73.63%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
-    bool dpProblem(int ind1, int ind2, string& s, string& p,
-                   vector<vector<int>>& dp) {
-        if (ind1 == s.size() && ind2 == p.size())
-            return true;
-        if (ind2 == p.size())
-            return false;    
-
-        if (ind1 == s.size()) {
-            for (int i = ind2; i < p.size(); i++) {
-                if (p[i] != '*')
-                    return false;
-            }
-            return true;
-        }
-
-         if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-        bool ans = false;
-
-        if (p[ind2] == '?') {
-            ans = dpProblem(ind1 + 1, ind2 + 1, s, p, dp);
-        } else if (p[ind2] == '*') {
-            ans = dpProblem(ind1 + 1, ind2, s, p, dp) ||
-                 dpProblem(ind1 + 1, ind2 + 1, s, p, dp) ||
-                  dpProblem(ind1, ind2 + 1, s, p, dp);
-        } else {
-            if (p[ind2] != s[ind1])
-                ans = false;
-            else
-                ans = dpProblem(ind1+1, ind2+1, s, p, dp);
-        }
-
-        return dp[ind1][ind2] = ans;
-    }
-
     bool isMatch(string s, string p) {
         int n1 = s.size();
         int n2 = p.size();
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+        
+        vector<vector<bool>> dp(n1 + 1, vector<bool>(n2 + 1, false));
+        
+        dp[0][0] = true; // empty string matches empty pattern
 
-        return dpProblem(0, 0, s, p, dp);
+        // Fill first row (string is empty)
+        for (int j = 1; j <= n2; j++) {
+            if (p[j - 1] == '*')
+            //  I thought this one
+              //  dp[0][j] = true
+                dp[0][j] = dp[0][j - 1];
+        }
+
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j]     // '*' matches one or more
+                             || dp[i][j - 1];   // '*' matches empty
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[n1][n2];
     }
 };
