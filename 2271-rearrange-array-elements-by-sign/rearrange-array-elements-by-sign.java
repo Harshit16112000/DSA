38aa3c8a08bdc6bb4764115e723809973162b6b0
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2026-04-29 22:01:26 +0530

    Time: 5 ms (23.58%) | Memory: 176.9 MB (5.06%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,28 +1,36 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        ArrayList<Integer> pos = new ArrayList<>();
-        ArrayList<Integer> neg = new ArrayList<>();
-        
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i] > 0)
-                pos.add(nums[i]);
-            else
-                neg.add(nums[i]);
-        }
+       int result[] = new int[nums.length];
+       int even[] = new int[nums.length/2];
+       int odd[] = new int[nums.length/2];
+       
+       int e = 0;
+       int o=0;
+       for(int i=0;i<nums.length;i++)
+       {
+            if(nums[i] > 0) {
+               even[e] = nums[i];
+               e++;
+            }
+            else {
+                odd[o] = nums[i];
+                o++;
+            }
+       } 
 
-        
-        int[] result = new int[nums.length];
-        int index = 0;
-        int i=0;
-        while(i < nums.length)
-        {
-            result[i] = pos.get(index);
-            i++;
-            result[i] = neg.get(index);
-            i++;
-            index++;
-        }
-        return result;
+        e=0;
+        o=0;
+       for(int i=0;i<nums.length;i++)
+       {
+            if(i%2 == 0) {
+                result[i] = even[e];
+                e++;
+            }  
+            else  {
+                result[i] = odd[o];
+                o++;
+            } 
+       }
+       return result;
     }
 }
