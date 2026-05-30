e3f46d34590d51d0c58a1931af99e22b5367f780
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2026-05-30 14:32:53 +0530

    Time: 4 ms (75.78%) | Memory: 45.4 MB (21.53%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,27 +1,39 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int ans[] = new int[nums1.length];
-        boolean flag = false;
-        for(int i=0;i<nums1.length;i++)
+        int ans[] = new int[nums2.length];
+        HashMap<Integer,Integer> hmap = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+        int result[] = new int[nums1.length];
+
+        for(int i=nums2.length-1;i>=0;i--)
         {
-            ans[i] = -1;
-            flag = false;
-            for(int j=0;j<nums2.length;j++)
+            if(st.empty())   ans[i] = -1;
+            else if(!st.empty() && st.peek() > nums2[i])
             {
-                if(flag) {
-                    if(nums1[i] < nums2[j])
-                    {
-                        ans[i] = nums2[j];
-                        break;
-                    }
-                }
-                if(nums1[i] == nums2[j])
-                {
-                    flag = true;
-                }
+                ans[i] = st.peek();
             }
+            else if(!st.empty() && st.peek() < nums2[i])
+            {
+                while(!st.empty() && st.peek() < nums2[i])
+                {
+                    st.pop();
+                }
+                if(st.empty())  ans[i] = -1;
+                else   ans[i] = st.peek();
+            }
+            st.push(nums2[i]);
+
         }
 
-        return ans;
+        for(int i=0;i<nums2.length;i++)
+        {
+            hmap.put(nums2[i], ans[i]);
+        }
+
+        for(int i=0;i<nums1.length;i++)
+        {
+            result[i] = hmap.get(nums1[i]);
+        }
+        return result;
     }
 }
