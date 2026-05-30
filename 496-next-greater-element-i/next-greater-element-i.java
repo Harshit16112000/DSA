745b42417c8b3a068d05d1ab2aff0ad64e4e9b17
Harshit16112000 <61346369+Harshit16112000@users.noreply.github.com>
2026-05-30 15:04:29 +0530

    Time: 5 ms (15.51%) | Memory: 45.4 MB (35.43%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int ans[] = new int[nums2.length];
         HashMap<Integer,Integer> hmap = new HashMap<>();
         Stack<Integer> st = new Stack<>();
         int result[] = new int[nums1.length];
 
         for(int i=nums2.length-1;i>=0;i--)
         {
-            if(st.empty())   ans[i] = -1;
+            if(st.empty())   hmap.put(nums2[i], -1);
             else if(!st.empty() && st.peek() > nums2[i])
             {
-                ans[i] = st.peek();
+                hmap.put(nums2[i], st.peek());
             }
             else if(!st.empty() && st.peek() < nums2[i])
             {
@@ -18,16 +17,11 @@ class Solution {
                 {
                     st.pop();
                 }
-                if(st.empty())  ans[i] = -1;
-                else   ans[i] = st.peek();
+                if(st.empty())  hmap.put(nums2[i], -1);
+                else   hmap.put(nums2[i], st.peek());
             }
             st.push(nums2[i]);
 
-        }
-
-        for(int i=0;i<nums2.length;i++)
-        {
-            hmap.put(nums2[i], ans[i]);
         }
 
         for(int i=0;i<nums1.length;i++)
